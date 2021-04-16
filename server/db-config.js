1dae155ea1cb661bd9bdaf9d15dd43466ac439bc
Sidney Pho <phosidney@gmail.com>
2021-04-16 15:29:51 -0600

    Delete db-config.js

--- server/db-config.js
+++
@@ -1,8 +0,0 @@
-// Private. Will not be included in submission
-module.exports = {
-  host: "finalprojectsecondattempt.cocgazgnyzld.us-east-1.rds.amazonaws.com",
-  port: "3306",
-  user: "admin",
-  password: "teamairbnb2021",
-  database: "airbnb"
-};
