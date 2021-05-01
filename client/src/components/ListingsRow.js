ef7e329a5b27783dbd27561d9ed9cdd3a6b441a6
MichelleGeng <Michelle.x.geng@gmail.com>
2021-05-01 04:22:03 -0700

    Updated Listings but still cant get it to show on client end

---
+++ client/src/components/ListingsRow.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../style/Listings.css';
+
+export default class ListingsRow extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="listingsResults">
+                <div className="zipcode">{this.props.zipcode}</div>
+				<a href={this.props.url} target="_blank">
+				<div className="listing_name">{this.props.name}</div>
+                </a>
+				<div className="neighborhood">{this.props.neighborhood}</div>
+				<div className="amenities">{this.props.amenities}</div>
+			</div>
+		);
+	}
+}
