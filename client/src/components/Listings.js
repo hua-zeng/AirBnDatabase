b490c38d11f5672a7a6b8cdfe960c992587c6bfa
MichelleGeng <Michelle.x.geng@gmail.com>
2021-05-01 15:36:59 -0700

    Adding updated Listings.js

--- client/src/components/Listings.js
+++ client/src/components/Listings.js
@@ -1,116 +1,99 @@
 import React from 'react';
 import PageNavbar from './PageNavbar';
-import BestGenreRow from './BestGenreRow';
+import ListingsRow from './ListingsRow';
 import '../style/Listings.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// Use Homework 2 to figure out how to create the drop-down, create pseudo coding
-
-export default class Listings extends React.Component { // copy this from Homework 2
+export default class Reviews extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {
-			selectedDecade: "",
-			decades: [],
-			genres: []
-		};
-
-		this.submitDecade = this.submitDecade.bind(this);
-		this.handleChange = this.handleChange.bind(this);
+		// should not prepopulate but am doing so to test
+		this.state = { 
+			writtenAmenities: "TV",
+			writtenZipcode: 94117, // should this be string or int?
+			listingsKey: []
+		}
+		this.handleWrittenAmenitiesChange = this.handleWrittenAmenitiesChange.bind(this);
+		this.handleWrittenZipcodeChange = this.handleWrittenZipcodeChange.bind(this);
+		this.submitListings = this.submitListings.bind(this);
 	}
 
-	// query database to get the info
-	componentDidMount() {
-		fetch("http://localhost:8081/decades", // change this to zipcodes or whatever the name of the file is
-		{
-		  method: 'GET' 
-		}).then(res => {
-		  return res.json();
-		}, err => {
-		  console.log(err);
-		}).then(decList => {
-		  if (!decList) return;
-
-		  let allDecades = decList.map((decObj, i) =>
-		  <option value={decObj.decade}>{decObj.decade}</option>
-		  );
 	
-		  this.setState({
-			decades: allDecades
-		  });
-		}, err => {
-		  console.log(err);
-		});
-	}
-
-	handleChange(e) {
+	handleWrittenAmenitiesChange(e) {
 		this.setState({
-			selectedDecade: e.target.value
+			writtenAmenities: e.target.value
+		});
+	}
+	
+	handleWrittenZipcodeChange(e) {
+		this.setState({
+			writtenZipcode: e.target.value
 		});
 	}
 
+	submitListings() {
+		fetch("http://localhost:8081/listings/" + this.state.writtenZipcode +"/"+this.state.writtenAmenities,
+    {
+      method: 'GET'
+    }).then(res => {
+      return res.json();
+    }, err => {
+      console.log(err);
+    }).then(listingsList => {
+      if (!listingsList) return;
+      let listingDivs = listingsList.map((listingsObj, i) =>
+	
+	  // DO I NEED TO ADD key={i} here? I don't think so because reviews doesn't but location does
+      <ListingsRow zipcode={listingsObj.zipcode} url = {listingsObj.url} name={listingsObj.name} neighborhood={listingsObj.neighborhood} amenities={listingsObj.amenities} />
+      );
 
-	submitDecade() { // one is a drop-down and the other is submit
-		fetch("http://localhost:8081/listings/",
-		{
-		  method: 'GET'
-		}).then(res => {
-		  return res.json();
-		}, err => {
-		  console.log(err);
-		}).then(moviesList => {
-		  if (!moviesList) return;
-		  let movie = moviesList.map((moviesObj, i) =>
-	
-		  <BestGenreRow genre={moviesObj.genre} rating={moviesObj.avg_rating} />
-		  );
-		  
-	
-		  this.setState({
-			  genres: movie
-		  });
-		}, err => {
-		  // Print the error if there is one.
-		  console.log(err);
-		});	
+      this.setState({
+		listingsKey: listingDivs
+	  });
+    }, err => {
+      // Print the error if there is one.
+      console.log(err);
+    });
 	}
 
-	render() { // render is the main method for the page, it's where everything shows up
-
+	
+	render() {
 		return (
+			// do I need to change the reviews name for the dividers?
 			<div className="Listings">
-				<PageNavbar active="listings" />
+				<PageNavbar active="Listings" />
 				<div style={{backgroundColor: 'white', minHeight:1000}}>
-
-				<div className="container listings-container">
-			      <div className="jumbotron">
-			        <div className="h5">LISTINGS</div>
-					<div> To see information about listings, please select attributes
-			        <div className="years-container">
-			          <div className="dropdown-container">
-			            <select value={this.state.selectedDecade} onChange={this.handleChange} className="dropdown" id="decadesDropdown">
-			            	<option select value> -- select a thing -- </option>
-			            	{this.state.decades} {/* this.state.decades refers to something above the page, change to this.state.zipcode */}
-			            </select>
-			            <button className="submit-btn" id="decadesSubmitBtn" onClick={this.submitDecade}>Submit</button>
-			          </div>
-			        </div>
-			      </div>
-			      <div className="jumbotron">
-			        <div className="movies-container">
-			          <div className="movie">
-			            <div className="header"><strong>Listing info</strong></div>
-			            <div className="header"><strong>Listings</strong></div>
-			          </div>
-			          <div className="movies-container" id="results">
-			            {this.state.genres}
-			          </div>
-			        </div>
-			      </div>
+			    <div className="container reviews-container">
+			    	<div className="jumbotron">
+			    		<div className="h5">LISTINGS</div>
+						<p>Please type in zipcode and <b>one</b> amenity to search for available listings that match the criteria.</p>
+			    		<br></br>
+						
+						<div className="zip-container">
+			    		<div className="input-container">
+							Please enter zipcode: &nbsp;
+							{/* Add back the placeholders. Do I need id and classname for the input tags for writtenAmenities? */}
+			    				<input type='text' placeholder="94117" value={this.state.writtenZipcode} onChange={this.handleWrittenZipcodeChange}  id="listings" className="listings-input"/>
+							 	&nbsp; &nbsp; &nbsp; ... and please enter amenity: &nbsp; <input type='text' value={this.state.writtenAmenities} onChange={this.handleWrittenAmenitiesChange} id="listings" className="listings-input"/>
+								<button id="submitListingsKeyBtn" className="submit-btn" onClick={this.submitListings}>Submit</button>
+			    			
+						</div>
+					</div>
+			    		<div className="header-container">
+			    			<div className="headers">
+										<div className="header"><strong>Zipcode</strong></div>
+										<div className="header"><strong>AirBnB Listing Name</strong></div>
+										<div className="header"><strong>Neighborhood</strong></div>
+										<div className="header"><strong>Amenities</strong></div>
+			    			</div>
+			    		</div>
+			    		<div className="results-container" id="results">
+			    			{this.state.listingsKey}
+			    		</div>
+			    	</div>
 			    </div>
-			</div>
-			</div>
+		    </div>
 			</div>
 		);
 	}
