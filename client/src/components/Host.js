4875e3698f1f38928f8a83d3a1e0f75d0796aabe
phosid <phosidney@gmail.com>
2021-04-30 20:22:29 -0500

    reset host.js

--- client/src/components/Host.js
+++ client/src/components/Host.js
@@ -3,41 +3,113 @@ import PageNavbar from './PageNavbar';
 import BestGenreRow from './BestGenreRow';
 import '../style/Host.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Bar} from 'react-chartjs-2';
 
-const state = {
-  labels: ['January', 'February', 'March',
-           'April', 'May'],
-  datasets: [
-    {
-      label: 'Rainfall',
-      backgroundColor: 'rgba(75,192,192,1)',
-      borderColor: 'rgba(0,0,0,1)',
-      borderWidth: 2,
-      data: [65, 59, 80, 81, 56]
-    }
-  ]
-}
+export default class Host extends React.Component {
+	constructor(props) {
+		super(props);
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Bar
-          data={state}
-          options={{
-            title:{
-              display:true,
-              text:'Average Rainfall per month',
-              fontSize:20
-            },
-            legend:{
-              display:true,
-              position:'right'
-            }
-          }}
-        />
-      </div>
-    );
-  }
+		this.state = {
+			selectedDecade: "",
+			decades: [],
+			genres: []
+		};
+
+		this.submitDecade = this.submitDecade.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+
+	componentDidMount() {
+		fetch("http://localhost:8081/decades",
+		{
+		  method: 'GET' 
+		}).then(res => {
+		  return res.json();
+		}, err => {
+		  console.log(err);
+		}).then(decList => {
+		  if (!decList) return;
+
+		  let allDecades = decList.map((decObj, i) =>
+		  <option value={decObj.decade}>{decObj.decade}</option>
+		  );
+	
+		  this.setState({
+			decades: allDecades
+		  });
+		}, err => {
+		  console.log(err);
+		});
+	}
+
+	handleChange(e) {
+		this.setState({
+			selectedDecade: e.target.value
+		});
+	}
+
+
+	submitDecade() {
+		fetch("http://localhost:8081/host/",
+		{
+		  method: 'GET'
+		}).then(res => {
+		  return res.json();
+		}, err => {
+		  console.log(err);
+		}).then(moviesList => {
+		  if (!moviesList) return;
+		  let movie = moviesList.map((moviesObj, i) =>
+	
+		  <BestGenreRow genre={moviesObj.genre} rating={moviesObj.avg_rating} />
+		  );
+		  
+	
+		  this.setState({
+			  genres: movie
+		  });
+		}, err => {
+		  // Print the error if there is one.
+		  console.log(err);
+		});	
+	}
+
+	render() {
+
+		return (
+			<div className="Host">
+				<PageNavbar active="host" />
+				<div style={{backgroundColor: 'white', minHeight:1000}}>
+
+				<div className="container host-container">
+			      <div className="jumbotron">
+			        <div className="h5">HOST</div>
+					<div> To see information about AirBnB Hosts and their listed properties, please select a target zipcode.
+			        <div className="years-container">
+			          <div className="dropdown-container">
+			            <select value={this.state.selectedDecade} onChange={this.handleChange} className="dropdown" id="decadesDropdown">
+			            	<option select value> -- select a zipcode -- </option>
+			            	{this.state.decades}
+			            </select>
+			            <button className="submit-btn" id="decadesSubmitBtn" onClick={this.submitDecade}>Submit</button>
+			          </div>
+			        </div>
+			      </div>
+			      <div className="jumbotron">
+			        <div className="movies-container">
+			          <div className="movie">
+			            <div className="header"><strong>Host info</strong></div>
+			            <div className="header"><strong>Listings</strong></div>
+			          </div>
+			          <div className="movies-container" id="results">
+			            {this.state.genres}
+			          </div>
+			        </div>
+			      </div>
+			    </div>
+			</div>
+			</div>
+			</div>
+		);
+	}
 }
